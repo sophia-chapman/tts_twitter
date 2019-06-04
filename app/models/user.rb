eb7a5b145e9e9873ced0c4b8cfbd4f748b5cffa3
Sophie Chapman <sophiechapman0808@gmail.com>
2019-06-03 20:44:30 -0500

    Created Epicenter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :tweets
 
+  serialize :following, Array
+
   mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_010634) do
+ActiveRecord::Schema.define(version: 2019_06_04_011759) do
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_010634) do
     t.text "bio"
     t.string "location"
     t.string "avatar"
+    t.text "following"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
